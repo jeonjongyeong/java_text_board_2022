41daefb9a21e9aeb1218c47589b4a42835b7b134
jkkjkhh <jkkjkhh@gmail.com>
2022-06-30 12:23:32 +0900

    등록시마다 게시물 번호가 증가

--- src/main/java/com/jjy/exam/board/Main.java
+++ src/main/java/com/jjy/exam/board/Main.java
@@ -6,11 +6,14 @@ public class Main {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
 
+    int articleLastId = 0;
+
     System.out.println("== 게시판 v 0.1 ==");
     System.out.println("== 프로그램 시작 ==");
     while (true) {
       System.out.printf("명령 ");
       String cmd = sc.nextLine();
+
       if (cmd.equals("exit")) {
         break;
       } else if (cmd.equals("/usr/article/write")) {
@@ -19,7 +22,8 @@ public class Main {
         String title = sc.nextLine();
         System.out.printf("내용 : ");
         String body = sc.nextLine();
-        int id = 1;
+        int id = articleLastId + 1;
+        articleLastId = id;
         System.out.printf("%d번 게시물이 입력 되었습니다. \n", id );
       } else {
         System.out.printf("입력된 명령어 : %s\n", cmd);
