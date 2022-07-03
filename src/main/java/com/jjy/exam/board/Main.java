24df128cd4d4988997e51f216dc79a6475c522fb
jkkjkhh <jkkjkhh@gmail.com>
2022-07-03 17:22:52 +0900

    articles, articlesLastId 변수를 static 변수로 승격

--- src/main/java/com/jjy/exam/board/Main.java
+++ src/main/java/com/jjy/exam/board/Main.java
@@ -25,20 +25,16 @@ TODO
 - [x] 생성자 도입
 */
 public class Main {
-    static void makeTestData(List<Article> articles) {
-        for (int i = 0; i < 100; i++) {
-            int id = i + 1;
-            articles.add(new Article(id, "제목" + id, "내용" + id));
-        }
-    }
+    static int articlesLastId = 0;
+    static List<Article> articles = new ArrayList<>();
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("== 게시판 v 0.1 ==");
         System.out.println("== 프로그램 시작 ==");
-        int articlesLastId = 0;
-        List<Article> articles = new ArrayList<>();
-        makeTestData(articles);
+
+        makeTestData();
+
         if (articles.size() > 0) {
             articlesLastId = articles.get(articles.size() - 1).id;
         }
@@ -50,12 +46,11 @@ public class Main {
             if (rq.getUrlPath().equals("exit")) {
                 break;
             } else if (rq.getUrlPath().equals("/usr/article/list")) {
-                actionUsrArticleList(rq, articles);
+                actionUsrArticleList(rq);
             } else if (rq.getUrlPath().equals("/usr/article/detail")) {
-                actionUsrArticleDetail(rq, articles);
+                actionUsrArticleDetail(rq);
             } else if (rq.getUrlPath().equals("/usr/article/write")) {
-                actionUsrArticleWrite(rq, sc, articles, articlesLastId);
-                articlesLastId++;
+                actionUsrArticleWrite(rq, sc);
             } else {
                 System.out.printf("입력된 명령어 : %s\n", cmd);
             }
@@ -64,23 +59,29 @@ public class Main {
         sc.close();
     }
 
-    private static void actionUsrArticleWrite(Rq rq, Scanner sc, List<Article> articles, int articlesLastId) {
-            System.out.println("- 게시물 등록 -");
-            System.out.printf("제목 : ");
-            String title = sc.nextLine();
-            System.out.printf("내용 : ");
-            String body = sc.nextLine();
-            int id = articlesLastId + 1;
-
-            Article article = new Article(id, title, body);
-            articles.add(article);
-            System.out.println("생성된 게시물 객체 : " + article);
-
-            System.out.printf("%d번 게시물이 입력되었습니다.\n", article.id);
+    static void makeTestData() {
+        for (int i = 0; i < 100; i++) {
+            int id = i + 1;
+            articles.add(new Article(id, "제목" + id, "내용" + id));
         }
     }
 
-    private static void actionUsrArticleDetail(Rq rq, List<Article> articles) {
+    private static void actionUsrArticleWrite(Rq rq, Scanner sc) {
+        System.out.println("- 게시물 등록 -");
+        System.out.printf("제목 : ");
+        String title = sc.nextLine();
+        System.out.printf("내용 : ");
+        String body = sc.nextLine();
+        int id = articlesLastId + 1;
+        articlesLastId = id;
+        Article article = new Article(id, title, body);
+        articles.add(article);
+        System.out.println("생성된 게시물 객체 : " + article);
+
+        System.out.printf("%d번 게시물이 입력되었습니다.\n", article.id);
+    }
+
+    private static void actionUsrArticleDetail(Rq rq) {
         Map<String, String> params = rq.getParams();
 
         if (params.containsKey("id") == false) {
@@ -110,7 +111,7 @@ public class Main {
         System.out.printf("내용 : %s\n", article.body);
     }
 
-    private static void actionUsrArticleList(Rq rq, List<Article> articles) {
+    private static void actionUsrArticleList(Rq rq) {
         System.out.println("- 게시물 리스트 -");
         System.out.println("--------------------");
         System.out.println("번호 / 제목");
@@ -150,24 +151,29 @@ public class Main {
         }
     }
 }
+
 class Article {
     int id;
     String title;
     String body;
+
     Article(int id, String title, String body) {
         this.id = id;
         this.title = title;
         this.body = body;
     }
+
     @Override
     public String toString() {
         return String.format("{id: %d, title: \"%s\", body: \"%s\"}", id, title, body);
     }
 }
+
 class Rq {
     private String url;
     private String urlPath;
     private Map<String, String> params;
+
     // 필드추가가능
     // 수정가능
     Rq(String url) {
@@ -175,15 +181,18 @@ class Rq {
         urlPath = Util.getUrlPathFromUrl(this.url);
         params = Util.getParamsFromUrl(this.url);
     }
+
     // 수정가능, if문 금지
     public Map<String, String> getParams() {
         return params;
     }
+
     // 수정가능, if문 금지
     public String getUrlPath() {
         return urlPath;
     }
 }
+
 class Util {
     // 이 함수는 원본리스트를 훼손하지 않고, 새 리스트를 만듭니다. 즉 정렬이 반대인 복사본리스트를 만들어서 반환합니다.
     public static <T> List<T> reverseList(List<T> list) {
@@ -193,6 +202,7 @@ class Util {
         }
         return reverse;
     }
+
     static Map<String, String> getParamsFromUrl(String url) {
         Map<String, String> params = new HashMap<>();
         String[] urlBits = url.split("\\?", 2);
@@ -209,6 +219,7 @@ class Util {
         }
         return params;
     }
+
     static String getUrlPathFromUrl(String url) {
         return url.split("\\?", 2)[0];
     }
